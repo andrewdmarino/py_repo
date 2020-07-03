9245f5f7bc647d35795572dbac5c8548e9968c71
andrewdmarino <66701607+andrewdmarino@users.noreply.github.com>
2020-07-02 21:43:09 -0600

    commit all

--- Homework/Instructions/PyBank/Resources/bank2.py
+++ Homework/Instructions/PyBank/Resources/bank2.py
@@ -8,21 +8,16 @@ csv_output = open(csv_folder / "data_file.txt", 'w')
 total = 0
 prev_val=0
 current_val=0
-
 #def monthly_change(monthly_chg_value)>:  try to define a fuction call to calc the change
-
-
 with open(csvpath) as csvfile:
     #csvreader object loads csfile (budget_data.csv) and explicitly adds ',' delimiter
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader) #skip header
     
     for row in csvreader:
-        total+ =int(row[1]) 
-        prev_val = (int(row[-1])
+        #prev_val = (int(row[-1])
         current_val = (int(row[1])    
-        print (prev_val-monthly_val)
-            
+        #print (prev_val-monthly_val)
+        total+ =int(row[1])     
 
 print(total)
-    
