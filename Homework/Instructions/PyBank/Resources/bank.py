16ffb6971a0fd18ea4b8fd06030176de96bb6a01
andrewdmarino <66701607+andrewdmarino@users.noreply.github.com>
2020-07-03 00:07:00 -0600

    commit all

--- Homework/Instructions/PyBank/Resources/bank.py
+++ Homework/Instructions/PyBank/Resources/bank.py
@@ -3,26 +3,18 @@ from pathlib import Path
 import csv
 
 csv_folder = Path("Homework/Instructions/PyBank/Resources/")
-csvpath = Path( csv_folder / "budget_data.csv")
+csvpath = Path(csv_folder / "budget_data.csv")
 csv_output = open(csv_folder / "data_file.txt", 'w')
+
 total = 0
 prev_val=0
 current_val=0
+change_value=0
 
 #def monthly_change(monthly_chg_value)>:  try to define a fuction call to calc the change
 
 
 with open(csvpath) as csvfile:
-    #csvreader object loads csfile (budget_data.csv) and explicitly adds ',' delimiter
-    csvreader = csv.reader(csvfile, delimiter=',')
-    csv_header = next(csvreader) #skip header
-    
-    for row in csvreader:
-        total+ =int(row[1]) 
-        prev_val = (int(row[-1])
-        current_val = (int(row[1])    
-        print (prev_val-monthly_val)
-            
-
-print(total)
+    for row in csvfile
+        first_line= 
     
--- Homework/Instructions/PyBank/Resources/bank2.py
+++ Homework/Instructions/PyBank/Resources/bank2.py
@@ -4,20 +4,30 @@ import csv
 
 csv_folder = Path("Homework/Instructions/PyBank/Resources/")
 csvpath = Path( csv_folder / "budget_data.csv")
-csv_output = open(csv_folder / "data_file.txt", 'w')
+
 total = 0
 prev_val=0
-current_val=0
-#def monthly_change(monthly_chg_value)>:  try to define a fuction call to calc the change
-with open(csvpath) as csvfile:
-    #csvreader object loads csfile (budget_data.csv) and explicitly adds ',' delimiter
-    csvreader = csv.reader(csvfile, delimiter=',')
-    csv_header = next(csvreader) #skip header
-    
-    for row in csvreader:
-        #prev_val = (int(row[-1])
-        current_val = (int(row[1])    
-        #print (prev_val-monthly_val)
-        total+ =int(row[1])     
+row_count = 0
 
-print(total)
+#month_col = [0]
+#total_col = [1]
+
+with open(csvpath) as csvfile:
+  my_csv = csv.reader(csvfile, delimiter = ',')
+  csv_headings = next(my_csv)
+  #first_line = next(my_csv)
+  for row in my_csv:
+    #print (row_count)
+    #print(row)
+    #prev_val = int(row[row_count-1]) // this is the column number
+    prev_val = (row[1].index)
+    print(prev_val)
+    current_val = int(row[1])
+    total += int(row[1])
+    row_count + = 1
+    #print (row_count)
+#print(first_line[0])
+print ("\n", total)
+csv_output = open(csv_folder / "data_file.txt", 'w')
+total_text = f'total is {total} \n'
+csv_output.write (total_text)
