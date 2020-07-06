47b697e0e7342921a4d14fd5ea2ec6c3bf3c7a1d
andrewdmarino <66701607+andrewdmarino@users.noreply.github.com>
2020-07-06 17:06:20 -0600

    commit all

--- Homework/Instructions/PyBank/Resources/bank2.py
+++ Homework/Instructions/PyBank/Resources/bank2.py
@@ -10,18 +10,28 @@ prev_val=0
 row_count = 0
 max_val = 0
 val_chg = 0
-#month_col = [0]
-#total_col = [1]
+valuelist=[]
+max_val= 0
+min_val= 0
+month_col = []
+total_col = []
 
 with open(csvpath) as csvfile:
   my_csv = csv.reader(csvfile, delimiter = ',')
   csv_headings = next(my_csv)
   #first_line = next(my_csv)
+  month_col = [0]
+  total_col = [1]
   for row in my_csv:
+    month_col.append(int(row[1]))
+    max_val=max(month_col)
+    min_val=min(month_col)
+    print(max_val, min_val)
+    #print(valuelist)
     current_val = int(row[1])
     prev_val = next(my_csv)[1]  
-    #print(prev_val)
     #print(current_val)
+    #print(prev_val)
     total += int(row[1])
     #print(max_val)
     #row_count + = 1
@@ -31,3 +41,7 @@ with open(csvpath) as csvfile:
 csv_output = open(csv_folder / "data_file.txt", 'w')
 total_text = f'total is {total} \n'
 csv_output.write (total_text)
+max_text = f'greatest monthly change is {max_val} \n'
+csv_output.write (max_text)
+min_text = f'greatest mnonthly loss is {min_val} \n'
+csv_output.write (min_text)
