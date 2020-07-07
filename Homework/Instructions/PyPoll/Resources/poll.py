4881e8e8e9c25cbbf8a1a86069a52e49ba7611af
andrewdmarino <66701607+andrewdmarino@users.noreply.github.com>
2020-07-06 21:57:43 -0600

    commit all

--- Homework/Instructions/PyPoll/Resources/poll.py
+++ Homework/Instructions/PyPoll/Resources/poll.py
@@ -1,25 +1,40 @@
-# First we'll import the os module
-# This will allow us to create file paths across operating systems
 import os
-
-# Module for reading CSV files
+from pathlib import Path
 import csv
 
-csvpath = os.path.join('..', 'Resources', 'accounting.csv')
-
-# read csv
+csv_folder = Path("Homework/Instructions/PyPoll/Resources/")
+csvpath = Path( csv_folder / "election_data.csv")
 
 with open(csvpath) as csvfile:
+    my_csv = csv.reader(csvfile, delimiter = ',')
+    csv_header = next(my_csv)
+    voter_id = []
+    county = []
+    candidate = []
+    khan_votes = 0
+    corey_votes = 0
+    li_votes = 0
+    oleary_votes = 0
+    loop_ctr = 0
+    print(csv_header)
 
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-    print(csvreader)
-
-    # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
-    # Read each row of data after the header
-    for row in csvreader:
-        print(row)
+    for row in my_csv:
+        voter_id = row[0]
+        county = row[1]
+        candidate = row[2]
+        #print (candidate) works 
+        if (candidate == 'Khan'):
+            khan_votes +=1 
+            loop_ctr+=1
+        #elif (candidate == 'Corey'): 
+        #    corey_votes +=1
+        #elif (candidate == 'Li'):
+        #    li_votes + = 1
+        #else:
+        #    oleary_votes+ =1
+            
+    print(khan_votes) #2mill votes?
+    #print(corey_votes)
+    #print(li_votes)
+    #print(oleary_votes)  
+    print(loop_ctr)   
