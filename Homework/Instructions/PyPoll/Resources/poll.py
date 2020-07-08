e5b52a68ce7c3614685a574c7d51de01335cfd42
andrewdmarino <66701607+andrewdmarino@users.noreply.github.com>
2020-07-07 18:14:42 -0600

    commit all

--- Homework/Instructions/PyPoll/Resources/poll.py
+++ Homework/Instructions/PyPoll/Resources/poll.py
@@ -36,37 +36,39 @@ with open(csvpath) as csvfile:
             oleary_votes +=1
 
     total_votes = khan_votes+correy_votes+li_votes+oleary_votes
-    #output to terminal output / round up with math.ceil?
+    #output to terminal output / round to nearest % 
     print("khan votes = ", khan_votes) 
-    print("khan won ", ((khan_votes/total_votes)*100), "percentage of votes")
+    print("khan recieved ", ((khan_votes/total_votes)*100), "percentage of votes")
     print("correy votes = ", correy_votes)
-    print("correy won ", ((correy_votes/total_votes)*100), "percentage of votes")
+    print("correy recieved ", ((correy_votes/total_votes)*100), "percentage of votes")
     print("li votes = ", li_votes)
-    print("li won ", ((li_votes/total_votes)*100), "percentage of votes")
-    print("oleary votes = ",oleary_votes)  
-    print("O'leary won", ((oleary_votes/total_votes)*100), "percentage of votes")
+    print("li recieved ", ((li_votes/total_votes)*100), "percentage of votes")
+    print("O'leary recieved", ((oleary_votes/total_votes)*100), "percentage of votes")
     print("total votes cast= ", total_votes)
 
     khan_percent = (round((khan_votes/total_votes)*100, 2)) 
-    #print(khan_percent)
-    correy_percent =((correy_votes/total_votes)*100)
-    li_percent = ((li_votes/total_votes)*100)
-    oleary_percent = ((oleary_votes/total_votes)*100)
+    correy_percent =(round((correy_votes/total_votes)*100, 2))
+    li_percent = (round((li_votes/total_votes)*100, 2))
+    oleary_percent = (round((oleary_votes/total_votes)*100, 2))
     
 
 csv_output = open(csv_folder / "data_file.txt", 'w')
+
 total_text = f'total votes cast {total_votes} \n'
 spacer_text =f'--------------------\n'
 csv_output.write(spacer_text)
 csv_output.write (total_text)
 csv_output.write(spacer_text)
-khan_text = int(khan_percent)
-csv.output = (khan_text)
- 
-    
-winner_text = f'and the winner of the election with {khan_votes} votes is Khan\n'
+khan_text = f'Khan received {khan_percent} percent of the vote \n'
+csv_output.write(khan_text)
+correy_text = f'Correy received {correy_percent} percent of the vote \n'
+csv_output.write (correy_text)
+li_text = f'Li received {li_percent} percent of the vote \n'
+csv_output.write(li_text)
+oleary_text = f'Oleary received {oleary_percent} percent of the vote \n'
+csv_output.write(oleary_text)
+csv_output.write(spacer_text)
+winner_text = f'and the winner of the election with {khan_votes} votes is Khan with {khan_percent} percent of the total votes \n'
 csv_output.write (winner_text)
 
-
-
- 
+#should just print the text vars to terminal here
